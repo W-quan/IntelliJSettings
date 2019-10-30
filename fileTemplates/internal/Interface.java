5d51207e5d0dd6f3bd98452d47a1daa8ef7fccab
wzq <863102331@qq.com>
2019-10-30 11:02:40 +0800

    WS-2019.1.1 <wzq@wzqdeMacBook-Pro.local Update baseRefactoring.xml, vim_settings.xml, ide.general.xml, project.default.xml, diff.xml, Singleton.java, Enum.java, Default copy.xml, Interface.java, yiiguxing.translation.xml, Class.java, Default.xml, WzqEditorConfig.icls, Default _1_.xml, notifications.xml, keymap.xml, debugger.xml, AnnotationType.java	Create markdown.xml, Mac OS X 10_5_ copy.xml, jdk.table.xml, updates.xml

--- fileTemplates/internal/Interface.java
+++ fileTemplates/internal/Interface.java
@@ -3,5 +3,6 @@
 #if (${IMPORT_BLOCK} != "")${IMPORT_BLOCK}
 #end
 #parse("File Header.java")
+
 #if (${VISIBILITY} == "PUBLIC")public #end interface ${NAME} #if (${INTERFACES} != "")extends ${INTERFACES} #end {
 }
